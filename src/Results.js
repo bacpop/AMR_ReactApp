76a4ad4505043c21c5bc728a22317d832efadfd3
muppi1993 <clara.gronemeyer20@imperial.ac.uk>
2021-12-02 13:06:02 +0000

    changes to table layout

--- src/Results.js
+++ src/Results.js
@@ -2,46 +2,28 @@ import React from "react";
 
 function Results(props){
 
-    //console.log(JSON.parse(result[0]));
-
     const results = props.resArr;
     const antibiotics = Object.keys(JSON.parse(results[0]));
     
-
     const tableItems =[];
 
     for(var i = 0; i <results.length; i++){
-        //const strain = JSON.parse(results[i])
-        
         const newStrain = antibiotics.map((antibiotic) =>  
-            <td>{JSON.parse(results[i])[antibiotic]}</td>
+            <td key={antibiotic}>{JSON.parse(results[i])[antibiotic]}</td>
         );
-        tableItems.push(<tr>{newStrain}</tr>);
+        tableItems.push(<tr key = {JSON.parse(results[i])["Penicillin"]}>{newStrain}</tr>); // change key to filename later!!
     }
-
-    /*
-    const tableItems = results.map( function(result){
-        return(
-            <tr>
-                result.map(function(item){return(<td>"text"</td>)}) 
-            </tr>
-        )
-    }   
-    );
-*/
 
     const head = antibiotics.map((antibiotic) =>  
         <th key={antibiotic}>{antibiotic}</th>
     );
 
     return(
-        <table>
+        <table class="center">
             <thead><tr>{head}</tr></thead>
             <tbody>{tableItems}</tbody>
         </table>
     )
 }
 
-
 export default Results;
-//<tbody>{tableItems}</tbody>
