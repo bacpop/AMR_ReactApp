3d2e0291d76c193ea282c8a6b9b0c0a7e1867c4f
muppi1993 <clara.gronemeyer20@imperial.ac.uk>
2021-12-08 11:53:13 +0000

    shading of table cells based on colours, BAs displayed on hover

--- src/Results.js
+++ src/Results.js
@@ -10,11 +10,11 @@ function Results(props){
     for(let i = 0; i <results.length; i++){
         //remove length from array
         const status = results[i]["length"];
-        //console.log(status);
+        const blue = 100;
         var newStrain = antibiotics.slice(0,-1).map((antibiotic) =>  
-            <td key={antibiotic}>{results[i][antibiotic]}<span className="CellComment">This sequence is of unusual length for S.pneumoniae</span></td>
+            <td style= {{background:"rgb(255,"+(1-results[i][antibiotic])*255+","+(1-results[i][antibiotic])*255+")" }} key={antibiotic}>{results[i][antibiotic]}<span className='CellComment'>This sequence has an unusual length for S. pneumoniae</span></td> //this comment is only printed for rows with className="CellWithComment"
         );
-        //make row red only  if length was false! <tr style="background-color:#FF0000" ...
+        //make row red only  if length is false
         if(status===true){
             tableItems.push(<tr key = {results[i]["filename"]}>{newStrain}</tr>);
         }
@@ -22,13 +22,22 @@ function Results(props){
 
     }
 
-    const head = antibiotics.slice(0,-1).map((antibiotic) =>  
-        <th key={antibiotic}>{antibiotic}</th>
-    );
+    //texts for tooltips
+    let text_Penicillin ="Balanced Accuracy on test datasets: Massachusetts: 0.933 Maela: 0.836";
+    let text_Chloramphenicol ="Balanced Accuracy on test dataset: Maela: 0.819";
+    let text_Tetracycline ="Balanced Accuracy on test datasets: Massachusetts: 0.953 Maela: 0.940";
+    let text_Trim_sulfa ="Balanced Accuracy on test datasets: Massachusetts: 0.706 Maela: 0.740";
+    let text_Erythromycin ="Balanced Accuracy on test datasets: Massachusetts: 0.959 Maela: 0.961";
+
+    const head = [
+        <th key={antibiotics[0]}>{antibiotics[0]}</th>, //do 'filename' seperately to not include tooltip
+        antibiotics.slice(1,-1).map((antibiotic) =>  
+        <th key={antibiotic}><div className="tooltip" >{antibiotic}<span className='tooltiptext'>{eval("text_"+antibiotic)}</span></div></th>
+    )]
 
     return(
         <table className="center">
-            <thead><tr>{head}</tr></thead>
+            <thead><tr className="CellWithComment">{head}</tr></thead>
             <tbody>{tableItems}</tbody>
         </table>
     )
